e942a68a20f09d20d403093988d0e1bffba83475
Renato Priolo <rpriolo@gmail.com>
2022-05-24 11:25:57 -0300

    Inserido o primeiro vendedor e mais registros na tabela de produtos

--- Introducao-ao-SQL.sql
+++ Introducao-ao-SQL.sql
@@ -31,3 +31,83 @@ CREATE TABLE TB_PRODUTOS (
     PRECO_LISTA FLOAT
 );
 
+INSERT INTO TB_PRODUTOS (
+    PRODUTO,
+    NOME,
+    EMBALAGEM,
+    TAMANHO,
+    SABOR,
+    PRECO_LISTA
+)
+VALUES (
+    '1040107',
+    'Light - 350 ml - Melancia',
+    'Lata',
+    '350 ml',
+    'Melancia',
+    4.56
+);
+
+SELECT * FROM TB_PRODUTOS;
+
+INSERT INTO TABELA_DE_VENDEDORES (
+    MATRICULA,
+    NOME,
+    PERCENTUAL_COMISSAO
+) VALUES (
+    '00233',
+    'José Geraldo da Fonseca',
+    0.10
+);
+
+SELECT * FROM TABELA_DE_VENDEDORES;
+
+INSERT INTO TB_PRODUTOS (
+    PRODUTO,
+    NOME,
+    EMBALAGEM,
+    TAMANHO,
+    SABOR,
+    PRECO_LISTA
+) VALUES (
+    '1037797',
+    'Clean - 2 Litros - Laranja',
+    'PET',
+    '2 Litros',
+    'Laranja',
+    16.01
+);
+
+INSERT INTO TB_PRODUTOS (
+    PRODUTO,
+    NOME,
+    EMBALAGEM,
+    TAMANHO,
+    SABOR,
+    PRECO_LISTA
+) VALUES (
+    '1000889',
+    'Sabor da Montanha - 700 ml - Uva',
+    'Garrafa',
+    '700 ml',
+    'Uva',
+    6.31
+);
+
+INSERT INTO TB_PRODUTOS (
+    PRODUTO,
+    NOME,
+    EMBALAGEM,
+    TAMANHO,
+    SABOR,
+    PRECO_LISTA
+) VALUES (
+    '1004327',
+    'Videira do Campo - 1,5 Litros - Melancia',
+    'PET',
+    '1,5 Litros',
+    'Melancia',
+    19.51
+);
+
+SELECT * FROM TB_PRODUTOS;
